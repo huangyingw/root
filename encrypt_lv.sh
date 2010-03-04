104d20cf5553d19bab653f74acab97dd477be1e6
huangyingw@PrimaryStorage <huangyingw@gmail.com>
2010-03-04 21:38:57 +0800

    No

--- encrypt_lv.sh
+++ encrypt_lv.sh
@@ -4,7 +4,8 @@ while read -r file; do
     mp=$file
     echo "cryptsetup --verbose --verify-passphrase -c aes-cbc-plain luksFormat $mp"
     echo "cryptsetup luksOpen $mp ${file#/dev/volgrp}"
-    echo "$file" | sed 's/volgrp/mapper/'
+    myval=`echo "$file" | sed 's/volgrp/mapper/'`
+    echo $myval
 
 done < <(lvdisplay | grep -o "/dev.*")
 
