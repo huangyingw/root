6571d626f28034a69806c3e40c3d7de2bf4fda8e
huangyingw@PrimaryStorage <huangyingw@gmail.com>
2010-03-04 21:19:59 +0800

    No

--- encrypt_lv.sh
+++ encrypt_lv.sh
@@ -5,9 +5,6 @@ lvdisplay | grep -o "/dev.*" > $lv_file
 
 cat $lv_file | while read file ; do
 	myval=`echo "$file" | sed 's/dev/media/'`
-	#mkdir -p "$myval"
-
-	#echo "$file" | sed 's/dev/media/' |	mkdir -p '{}'
-	#echo "$file" | sed 's/dev/media/'
+	echo $myval
 	#mkdir -p 'echo "$file" | sed 's/dev/media/''
 done
