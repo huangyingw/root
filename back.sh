613edad669cecd6326aef3043923fd1d08d20807
huangyingw@PrimaryStorage <huangyingw@gmail.com>
2010-03-20 22:50:14 -0400

    add move.sh

--- back.sh
+++ back.sh
@@ -1,21 +1,21 @@
 #! /bin/bash
 
 source=/media/volgrp/av/
-target=/media/archive2/av/
+target=/media/back1/av/
 
 rsync -av --recursive --delete -h --times --links --hard-links \
 			--stats --progress \
 			"${source}" "${target}"
 
 source=/media/volgrp/software/
-target=/media/archive3/software/
+target=/media/back1/software/
 
 rsync -av --recursive --delete -h --times --links --hard-links \
 			--stats --progress \
 			"${source}" "${target}"
 			
 source=/media/volgrp/unsafe/
-target=/media/archive3/unsafe/
+target=/media/back1/unsafe/
 
 rsync -av --recursive --delete -h --times --links --hard-links \
 			--stats --progress \
