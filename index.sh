8d65f9cad790c2c9699e1c254b2299b34fccc7e1
huangyingw@linux_programming <huangyingw@gmail.com>
2010-03-23 05:05:38 -0400

    add .profile file

---
+++ index.sh
@@ -0,0 +1,4 @@
+#!/bin/bash 
+#java -cp /media/myproject/git/java/lucene/lucene-3.0.1/lucene-core-3.0.1.jar:/media/myproject/git/java/lucene/lucene-3.0.1/lucene-demos-3.0.1.jar org.apache.lucene.demo.IndexFiles /media/myproject/git/java/lucene/lucene-3.0.1/docs/
+java org.apache.lucene.demo.IndexFiles /media/myproject/git/java/lucene/lucene-3.0.1/docs/
+
