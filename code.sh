9915ae9b102f6c0e43586a5d814344b0f0bbe64b
huangyingw@PrimaryStorage <huangyingw@gmail.com>
2010-05-06 10:45:12 -0400

    n

--- code.sh
+++ code.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
 
-source=/media/volgrp/av/
-target=/media/back1/av/
+source=/media/back2/myproject/
+target=/media/8G/myproject/
 
+rsync -av --recursive --delete -h --times --links --hard-links \
+			--stats --progress \
+			--exclude work \
+			"${source}" "${target}"
