073c7f011910956d299a08b959a651e87a69b32a
huangyingw@linux_programming <huangyingw@gmail.com>
2010-05-12 23:07:26 +0800

    n

--- rsync.sh
+++ rsync.sh
@@ -1,9 +1,14 @@
 #!/bin/bash 
-RSYNCSOURCE=$1
-BASEDIR=$2
+SOURCE=$1
+TARGET=$2
+
+if [ ! -d "${TARGET}" ];
+then   
+    mkdir -p "${TARGET}"
+fi
 
 rsync -av --recursive --delete -h --times --links --hard-links \
 			--stats --progress \
 			--exclude .svn \
-			"${RSYNCSOURCE}" "${BASEDIR}"
+			"${SOURCE}" "${TARGET}"
 
