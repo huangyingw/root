8eea3f78b584dc10d8fb8273c172b9f93ed900d2
huangyingw@PrimaryStorage <huangyingw@gmail.com>
2010-06-03 11:06:47 -0400

    n

--- check_disk.sh
+++ check_disk.sh
@@ -1,6 +1,13 @@
 #! /bin/bash
 check_file=/root/disk_list.txt
+result=1
 cat $check_file | while read file ; do
    string=`df -Th|grep ${file}`
-   echo ${string}
+   #echo ${string}
+		if [ -z string ]
+		then   
+    	result=0
+	 	fi
 done
+echo ${result}
+
