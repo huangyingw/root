403c5066a6cd9d8217f739a05cb82b0c476e533c
huangyingw@PrimaryStorage <huangyingw@gmail.com>
2010-06-03 11:20:38 -0400

    n

--- check_disk.sh
+++ check_disk.sh
@@ -2,12 +2,14 @@
 check_file=/root/disk_list.txt
 result=1
 cat $check_file | while read file ; do
-   string=`df -Th|grep ${file}`
+   #string=`df -Th|grep ${file}`
    #echo ${string}
-		if [ -z string ]
+		#if [ -e ${string} ]
+		if [ df -Th|grep ${file} ]
 		then   
     	result=0
 	 	fi
 done
 echo ${result}
 
+#(grep -c SOME_STRING $PATH/$LOGFILE) -ne 0
