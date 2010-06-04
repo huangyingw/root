887089556ad489250d91440414b9fcc4ce4beced
huangyingw@PrimaryStorage <huangyingw@gmail.com>
2010-06-04 11:45:29 -0400

    n

--- back.sh
+++ back.sh
@@ -17,104 +17,103 @@ echo $check_bool
 
 if [ ${check_bool} -eq 1 ];   
 then
-	echo the disks ready!!!
+	source=/media/volgrp/av/
+	target=/media/back1/av/
+	
+	rsync -av --recursive --delete -h --times --links --hard-links \
+				--stats --progress \
+				"${source}" "${target}"
+	
+	source=/media/volgrp/software/
+	target=/media/archive1/software/
+	
+	rsync -av --recursive --delete -h --times --links --hard-links \
+				--stats --progress \
+				"${source}" "${target}"
+				
+	source=/media/volgrp/unsafe/
+	target=/media/back1/unsafe/
+	
+	rsync -av --recursive --delete -h --times --links --hard-links \
+				--stats --progress \
+				"${source}" "${target}"
+	
+	source=/media/volgrp/xgmm/
+	target=/media/back2/xgmm/
+	
+	rsync -av --recursive --delete -h --times --links --hard-links \
+				--stats --progress \
+				"${source}" "${target}"
+	
+	source=/media/volgrp/cartoon/
+	target=/media/back2/cartoon/
+	
+	rsync -av --recursive --delete -h --times --links --hard-links \
+				--stats --progress \
+				"${source}" "${target}"
+	
+	source=/media/volgrp/movie/
+	target=/media/back2/movie/
+	
+	rsync -av --recursive --delete -h --times --links --hard-links \
+				--stats --progress \
+				"${source}" "${target}"
+	
+	source=/media/volgrp/myproject/
+	target=/media/back2/myproject/
+	
+	rsync -av --recursive --delete -h --times --links --hard-links \
+				--stats --progress \
+				"${source}" "${target}"
+	
+	source=/media/volgrp/storage/
+	target=/media/back2/storage/
+	
+	rsync -av --recursive --delete -h --times --links --hard-links \
+				--stats --progress \
+				"${source}" "${target}"
+				
+	source=/media/volgrp/vmware/
+	target=/media/back2/vmware/
+	
+	rsync -av --recursive --delete -h --times --links --hard-links \
+				--stats --progress \
+				"${source}" "${target}"
+	
+	source=/media/back2/myproject/
+	target=/media/back1/myproject/
+	
+	rsync -av --recursive --delete -h --times --links --hard-links \
+				--stats --progress \
+				"${source}" "${target}"
+	
+	source=/media/back2/myproject/
+	target=/media/archive1/myproject/
+	
+	rsync -av --recursive --delete -h --times --links --hard-links \
+				--stats --progress \
+				"${source}" "${target}"
+	
+	source=/media/back2/myproject/
+	target=/media/archive2/myproject/
+	
+	rsync -av --recursive --delete -h --times --links --hard-links \
+				--stats --progress \
+				"${source}" "${target}"
+	
+	source=/media/back2/myproject/
+	target=/media/archive3/myproject/
+	
+	rsync -av --recursive --delete -h --times --links --hard-links \
+				--stats --progress \
+				"${source}" "${target}"
+	
+	case "$1" in
+		s)
+			shutdown -h now
+			;;
+	esac
 else
  	echo the disks not ready!!!
 fi
 
-#source=/media/volgrp/av/
-#target=/media/back1/av/
-#
-#rsync -av --recursive --delete -h --times --links --hard-links \
-#			--stats --progress \
-#			"${source}" "${target}"
-#
-#source=/media/volgrp/software/
-#target=/media/archive1/software/
-#
-#rsync -av --recursive --delete -h --times --links --hard-links \
-#			--stats --progress \
-#			"${source}" "${target}"
-#			
-#source=/media/volgrp/unsafe/
-#target=/media/back1/unsafe/
-#
-#rsync -av --recursive --delete -h --times --links --hard-links \
-#			--stats --progress \
-#			"${source}" "${target}"
-#
-#source=/media/volgrp/xgmm/
-#target=/media/back2/xgmm/
-#
-#rsync -av --recursive --delete -h --times --links --hard-links \
-#			--stats --progress \
-#			"${source}" "${target}"
-#
-#source=/media/volgrp/cartoon/
-#target=/media/back2/cartoon/
-#
-#rsync -av --recursive --delete -h --times --links --hard-links \
-#			--stats --progress \
-#			"${source}" "${target}"
-#
-#source=/media/volgrp/movie/
-#target=/media/back2/movie/
-#
-#rsync -av --recursive --delete -h --times --links --hard-links \
-#			--stats --progress \
-#			"${source}" "${target}"
-#
-#source=/media/volgrp/myproject/
-#target=/media/back2/myproject/
-#
-#rsync -av --recursive --delete -h --times --links --hard-links \
-#			--stats --progress \
-#			"${source}" "${target}"
-#
-#source=/media/volgrp/storage/
-#target=/media/back2/storage/
-#
-#rsync -av --recursive --delete -h --times --links --hard-links \
-#			--stats --progress \
-#			"${source}" "${target}"
-#			
-#source=/media/volgrp/vmware/
-#target=/media/back2/vmware/
-#
-#rsync -av --recursive --delete -h --times --links --hard-links \
-#			--stats --progress \
-#			"${source}" "${target}"
-#
-#source=/media/back2/myproject/
-#target=/media/back1/myproject/
-#
-#rsync -av --recursive --delete -h --times --links --hard-links \
-#			--stats --progress \
-#			"${source}" "${target}"
-#
-#source=/media/back2/myproject/
-#target=/media/archive1/myproject/
-#
-#rsync -av --recursive --delete -h --times --links --hard-links \
-#			--stats --progress \
-#			"${source}" "${target}"
-#
-#source=/media/back2/myproject/
-#target=/media/archive2/myproject/
-#
-#rsync -av --recursive --delete -h --times --links --hard-links \
-#			--stats --progress \
-#			"${source}" "${target}"
-#
-#source=/media/back2/myproject/
-#target=/media/archive3/myproject/
-#
-#rsync -av --recursive --delete -h --times --links --hard-links \
-#			--stats --progress \
-#			"${source}" "${target}"
-#
-#case "$1" in
-#	s)
-#		shutdown -h now
-#		;;
-#esac
