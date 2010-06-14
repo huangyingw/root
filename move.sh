525ac077261dda93c8714a64956630fe6f49d157
huangyingw@linux_programming <huangyingw@gmail.com>
2010-06-14 04:44:47 -0400

    n

--- move.sh
+++ move.sh
@@ -2,6 +2,11 @@
 RSYNCSOURCE=$1
 RSYNCTARGET=$2
 
+if [ ! -d "${TARGET}" ];
+then   
+    mkdir -p "${TARGET}"
+fi
+
 rsync -av --recursive -h --times --links --hard-links \
 	--stats --progress \
 	"${RSYNCSOURCE}" "${RSYNCTARGET}" \
