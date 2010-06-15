7001d35b7ec2855994dd148df5d67d495ad49f02
huangyingw@PrimaryStorage <huangyingw@gmail.com>
2010-06-15 21:08:22 +0800

    n

--- rsync.sh
+++ rsync.sh
@@ -2,7 +2,7 @@
 RSYNCSOURCE=$1
 BASEDIR=$2
 
-rsync -av --recursive --delete -h --times --links --hard-links \
+rsync -av --log-file=/root/rlog --recursive --delete -h --times --links --hard-links \
 			--stats --progress \
 			"${RSYNCSOURCE}" "${BASEDIR}"
 
--- task.sh
+++ task.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
-./move.sh /media/archive1/vmware_archive/ /media/archive2/vmware_archive/
-./back.sh s
+./rsync.sh /media/archive1/vmware_archive/ /media/archive2/vmware_archive/
 if [ "$1" != s ];
 	then
 		shutdown -h now
