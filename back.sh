3e3816e7163fcb971439cb6aeb403f4441b0a099
huangyingw@PrimaryStorage <huangyingw@gmail.com>
2010-06-19 22:16:44 +0800

    n

--- back.sh
+++ back.sh
@@ -21,70 +21,60 @@ then
 	target=/media/back2/av/
 	
 	rsync -av --log-file=/root/rlog --recursive --delete -h --times --links --hard-links \
-				--stats --progress \
 				"${source}" "${target}"
 	
 	source=/media/volgrp/software/
 	target=/media/back1/software/
 	
 	rsync -av --log-file=/root/rlog --recursive --delete -h --times --links --hard-links \
-				--stats --progress \
 				"${source}" "${target}"
 	
 	source=/media/volgrp/unsafe/
 	target=/media/back2/unsafe/
 	
 	rsync -av --log-file=/root/rlog --recursive --delete -h --times --links --hard-links \
-				--stats --progress \
 				"${source}" "${target}"
 	
 	source=/media/volgrp/xgmm/
 	target=/media/archive3/xgmm/
 	
 	rsync -av --log-file=/root/rlog --recursive --delete -h --times --links --hard-links \
-				--stats --progress \
 				"${source}" "${target}"
 	
 	source=/media/volgrp/cartoon/
 	target=/media/back2/cartoon/
 	
 	rsync -av --log-file=/root/rlog --recursive --delete -h --times --links --hard-links \
-				--stats --progress \
 				"${source}" "${target}"
 	
 	source=/media/volgrp/movie/
 	target=/media/archive1/movie/
 	
 	rsync -av --log-file=/root/rlog --recursive --delete -h --times --links --hard-links \
-				--stats --progress \
 				"${source}" "${target}"
 	
 	source=/media/volgrp/myproject/
 	target=/media/back2/myproject/
 	
 	rsync -av --log-file=/root/rlog --recursive --delete -h --times --links --hard-links \
-				--stats --progress \
 				"${source}" "${target}"
 				
 	source=/media/volgrp/myproject/
 	target=/media/archive3/myproject/
 	
 	rsync -av --log-file=/root/rlog --recursive --delete -h --times --links --hard-links \
-				--stats --progress \
 				"${source}" "${target}"
 	
 	source=/media/volgrp/storage/
 	target=/media/back1/storage/
 	
 	rsync -av --log-file=/root/rlog --recursive --delete -h --times --links --hard-links \
-				--stats --progress \
 				"${source}" "${target}"
 				
 	source=/media/volgrp/vmware/
 	target=/media/archive2/vmware/
 	
 	rsync -av --log-file=/root/rlog --recursive --delete -h --times --links --hard-links \
-				--stats --progress \
 				"${source}" "${target}"
 	
 	if [ "$1" != s ];
