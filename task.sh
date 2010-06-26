79667bc5f0b209f9b0c11714ef8297ad53b66d63
huangyingw@PrimaryStorage <huangyingw@gmail.com>
2010-06-26 10:28:17 +0800

    n

--- task.sh
+++ task.sh
@@ -1,19 +1,5 @@
 #! /bin/bash
-function check_disk
-{
-	check_file=/root/disk_list.txt
-	result=1
-	for file in `cat $check_file`
-	do
-		if ! (df -Th|grep -q ${file})
-		then   
-    	result=0
-    	echo ${file}>> check_disk.log
-	 	fi
-	done
-	echo ${result}
-}
-check_bool=`check_disk`
+check_bool=`./check_disk.sh`
 
 if [ ${check_bool} -eq 1 ];   
 then
