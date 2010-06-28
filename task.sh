1c69c4c2494e62e216d9ca9c8b2d8a0dba487e56
huangyingw@PrimaryStorage <huangyingw@gmail.com>
2010-06-28 22:23:33 +0800

    n

--- task.sh
+++ task.sh
@@ -4,6 +4,7 @@ check_bool=`./check_disk.sh`
 if [ ${check_bool} -eq 1 ];   
 then
 	./back.sh s
+	./move.sh /media/archive2_bak/vmware_archive/ /media/archive2/vmware_archive/
 	if [ "$1" != s ];
 		then
 			shutdown -h now
