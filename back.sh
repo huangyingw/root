96affc3494bdab31580e7756f63aec2d39a003d2
huangyingw@PrimaryStorage <huangyingw@gmail.com>
2010-06-30 21:59:31 +0800

    n

--- back.sh
+++ back.sh
@@ -5,7 +5,22 @@ if [ ${check_bool} -eq 1 ];
 then
 	source=/media/volgrp/
 	target=/media/volgrp_bak/
+	./rsync.sh	"${source}" "${target}"
 	
+	source=/media/volgrp/myproject/
+	target=/media/archive1/myproject/
+	./rsync.sh	"${source}" "${target}"
+	
+	source=/media/archive1/
+	target=/media/archive1_bak/
+	./rsync.sh	"${source}" "${target}"
+	
+	source=/media/volgrp/myproject/
+	target=/media/archive2/myproject/
+	./rsync.sh	"${source}" "${target}"
+	
+	source=/media/archive2/
+	target=/media/archive2_bak/
 	./rsync.sh	"${source}" "${target}"
 	
 	if [ "$1" != s ];
--- task.sh
+++ task.sh
@@ -4,7 +4,6 @@ check_bool=`./check_disk.sh`
 if [ ${check_bool} -eq 1 ];   
 then
 	./back.sh s
-	./move.sh /media/archive2_bak/vmware_archive/ /media/archive2/vmware_archive/
 	if [ "$1" != s ];
 		then
 			shutdown -h now
