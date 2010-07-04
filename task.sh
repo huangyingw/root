09cc205fda2169079c18df35c157d045dcde9b58
huangyingw@PrimaryStorage <huangyingw@gmail.com>
2010-07-04 18:10:17 +0800

    n

--- task.sh
+++ task.sh
@@ -4,6 +4,7 @@ check_bool=`./check_disk.sh`
 if [ ${check_bool} -eq 1 ];   
 then
 	./back.sh s
+	./move.sh /media/volgrp/av/move/ /media/archive1/av_archive/
 	if [ "$1" != s ];
 		then
 			shutdown -h now
