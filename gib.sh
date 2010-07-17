013b5164509c6781ef4fd555aab9bc203133c8f4
huangyingw@linux_programming <huangyingw@gmail.com>
2010-07-17 10:07:21 +0800

    n

--- gib.sh
+++ gib.sh
@@ -2,15 +2,19 @@
 if [ ! -d "temp" ];
 then
 	echo begin to do work!!!
-	if [ ! -d "$1" ];
-then
-	cd "$1"
-	git init
-	git add .
-	git commit -am "n"
+	if [ ! -d "$1/.git" ];
+	then
+		echo begin git init!!!
+		#cd "$1"
+		#git init
+		#git add .
+		#git commit -am "n"
     #git clone --bare "$1" "$2"
 		#rm -frv "$1"
 		#mv -fv "$2" "$1"
+	else
+ 		echo git exist!!!
+	fi
 else
  	echo the disks not ready!!!
 fi
