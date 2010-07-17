2cb03f25b3c95e91659287d2f0e963fddab69cc1
huangyingw@linux_programming <huangyingw@gmail.com>
2010-07-17 11:28:36 +0800

    n

--- gctb.sh
+++ gctb.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
-git clone --bare "$1" "$2"
-rm -frv "$1"
-mv -fv "$2" "$1"
+if [ -z "$1" ];
+then
+	echo "Please provide parameter"
+else
+	if [ ! -d "temp" ];
+	then
+		if [ ! -d "$1/.git" ];
+		then
+			echo git does not exist!!!
+		else
+	 		git clone --bare "$1" "temp"
+			rm -frv "$1"
+			mv -fv "temp" "$1"
+		fi
+	else
+	 	echo the temp exist!!!
+	fi
+fi
+	
+
