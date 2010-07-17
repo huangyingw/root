b184ce28f90cc9b7580d61f49e36bd2878136a72
huangyingw@linux_programming <huangyingw@gmail.com>
2010-07-17 10:03:04 +0800

    n

--- gib.sh
+++ gib.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-git clone --bare "$1" "$2"
-rm -frv "$1"
-mv -fv "$2" "$1"
+if [ ! -d "${temp}" ];
+then
+	echo begin to do work!!!
+    #git clone --bare "$1" "$2"
+		#rm -frv "$1"
+		#mv -fv "$2" "$1"
+else
+ 	echo the disks not ready!!!
+fi
