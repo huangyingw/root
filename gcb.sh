2300bd7192d2e11065a688b3e3f09d4a478795d6
huangyingw@linux_programming <huangyingw@gmail.com>
2010-08-06 23:47:08 -0400

    n

--- gcb.sh
+++ gcb.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
-if [ ! -d ".git" ];
-then
-	echo not a git repo!!!
-else
-	git clone --bare . /media/smb/
-	git remote rm origin
-	git remote add origin "$2"
-fi
+
+x=$(dirname "$0")
+echo ${x}
+x=`echo ${x} | sed -e 's/\/root\///'`
+x=/media/smb/${x}
+echo ${x}
+
+#if [ ! -d ".git" ];
+#then
+#	echo not a git repo!!!
+#else
+#	git clone --bare . /media/smb/
+#	git remote rm origin
+#	git remote add origin "$2"
+#fi
 
