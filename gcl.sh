346959512ae469d06962df8cc88d0a6c9392d388
huangyingw@linux_programming <huangyingw@gmail.com>
2010-09-03 05:02:42 -0400

    n

--- gcl.sh
+++ gcl.sh
@@ -10,17 +10,12 @@ fi
 
 
 
-#if [ ! -d ".git" ];
-#then
-#	echo not a git repo!!!
-#else
-#	if [ -d ${x} ];
-#	then
-#		echo remote exist!!!
-#	else
-#		git clone --bare . ${x}
-#		git remote rm origin
-#		git remote add origin ${x}
-#	fi
-#fi
-#
+if [ -d ${x} ];
+then
+	echo local repository exist!!!
+else
+	echo do nothing
+	#git clone --bare . ${x}
+	#git remote rm origin
+	#git remote add origin ${x}
+fi
