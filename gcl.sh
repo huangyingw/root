2dbd6b736c9c677fd8fcf80b45ca727dde3c83cd
huangyingw@linux_programming <huangyingw@gmail.com>
2010-09-03 04:58:21 -0400

    n

---
+++ gcl.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ -z "$1" ];
+then
+	echo "Please provide parameter"
+else
+	x=`echo $1 | sed -e 's/\/root\///;s/\/media\/smb\///'`
+	x=/root/${x}
+	echo ${x}
+fi
+
+
+
+#if [ ! -d ".git" ];
+#then
+#	echo not a git repo!!!
+#else
+#	if [ -d ${x} ];
+#	then
+#		echo remote exist!!!
+#	else
+#		git clone --bare . ${x}
+#		git remote rm origin
+#		git remote add origin ${x}
+#	fi
+#fi
+#
