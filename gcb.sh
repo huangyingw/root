3a2b6299ba483e3cdd968dc642405c762942f107
huangyingw@linux_programming <huangyingw@gmail.com>
2010-09-03 05:07:54 -0400

    n

--- gcb.sh
+++ gcb.sh
@@ -2,20 +2,21 @@
 
 x=`pwd`
 echo ${x}
-x=`echo ${x} | sed -e 's/\/root\///;s/\/media\/volgrp\///'`
-x=/media/smb/${x}
+x=`echo ${x} | sed -e 's/\/root\//\/media\/smb\//;s/\/media\/volgrp\//\/media\/smb\//'`
+#x=/media/smb/${x}
 echo ${x}
 
-if [ ! -d ".git" ];
-then
-	echo not a git repo!!!
-else
-	if [ -d ${x} ];
-	then
-		echo remote exist!!!
-	else
-		git clone --bare . ${x}
-		git remote rm origin
-		git remote add origin ${x}
-	fi
-fi
+#if [ ! -d ".git" ];
+#then
+#	echo not a git repo!!!
+#else
+#	if [ -d ${x} ];
+#	then
+#		echo remote exist!!!
+#	else
+#		git clone --bare . ${x}
+#		git remote rm origin
+#		git remote add origin ${x}
+#	fi
+#fi
+#
