05e2b95e1541028595af017bd02e90aa78c9d188
huangyingw@linux_programming <huangyingw@gmail.com>
2010-09-03 05:09:50 -0400

    n

--- gcl.sh
+++ gcl.sh
@@ -4,16 +4,16 @@ if [ -z "$1" ];
 then
 	echo "Please provide parameter"
 else
-	x=`echo $1 | sed -e 's/media\/smb/root/'`
+	x=`echo $1 | sed -e 's/\/media\/smb\//\/root\//'`
 	echo ${x}
 fi
 
 
 
-if [ -d ${x} ];
-then
-	echo local repository exist!!!
-else
-	#echo "git clone -v "$1" ${x}"
-	git clone -v "$1" ${x}
-fi
+#if [ -d ${x} ];
+#then
+#	echo local repository exist!!!
+#else
+#	#echo "git clone -v "$1" ${x}"
+#	git clone -v "$1" ${x}
+#fi
