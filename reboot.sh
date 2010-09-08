262a21069d883a5504a7031b661c832243fabc41
huangyingw@Mldonkey <huangyingw@gmail.com>
2010-09-08 10:35:57 -0400

    n

--- reboot.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-/etc/init.d/mldonkey-server stop
-cd /var
-git commit -am "n" && git push --all && git push --tags
-reboot
--- rsync.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash 
-RSYNCSOURCE=$1
-BASEDIR=$2
-
-rsync -av --recursive --delete -h --times --links --hard-links \
-			--stats --progress \
-			"${RSYNCSOURCE}" "${BASEDIR}"
-
