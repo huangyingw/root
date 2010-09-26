c58b22ad83a5accf87b4b3c95bb969c0ce7ce45f
huangyingw@linux_programming <huangyingw@gmail.com>
2010-09-26 19:05:14 +0800

    n

--- gps.sh
+++ gps.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-for ss in `git remote -v | grep -o '^[[:alnum:]]*' | uniq
-`; do git push --all $ss && git push --tag $ss; done
+for ss in `git remote -v | grep -o '^[[:alnum:]]*' | uniq`; do git push --all $ss && git push --tag $ss; done
