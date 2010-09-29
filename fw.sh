ade401e1e6cf29bbeae0693a8d1bb4d8a0183457
huangyingw@linux_programming <huangyingw@gmail.com>
2010-09-29 16:45:46 +0800

    n

--- fw.sh
+++ fw.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-find "$1" \( -name \.svn -o -name \.git -o -name \.hg \) -prune -o \( -name \*\.c -o -name \*\.cc -o -name \*\.h -o -name \*\.sh -o -name [m,M]akefile \) -exec grep -wnHA2 -B2 "$2" {} \;
+if [ -z "$3" ];
+then
+		find "$1" \( -name \.svn -o -name \.git -o -name \.hg \) -prune -o \( -name \*\.c -o -name \*\.cc -o -name \*\.h -o -name \*\.sh -o -name [m,M]akefile \) -exec grep -wnH  "$2" {} \;
+else
+		find "$1" \( -name \.svn -o -name \.git -o -name \.hg \) -prune -o \( -name \*\.c -o -name \*\.cc -o -name \*\.h -o -name \*\.sh -o -name [m,M]akefile \) -exec grep -wnHA$3 -B$3  "$2" {} \;
+fi
