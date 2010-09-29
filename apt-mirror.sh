8f2c313cb14787a9f3a9b9ff995d0606086d1f38
huangyingw@linux_programming <huangyingw@gmail.com>
2010-09-29 16:05:07 +0800

    n

--- apt-mirror.sh
+++ apt-mirror.sh
@@ -1,5 +1,4 @@
 #! /bin/bash
-echo $$ > /tmp/program.lock
 
 RSYNCSOURCE=rsync://mirrors.sohu.com/ubuntu/
 #RSYNCSOURCE=rsync://debian.nctu.edu.tw/ubuntu/
@@ -17,11 +16,20 @@ if [ -f /tmp/program.lock ] ; then
   else
 		  # process not running, but lock file not deleted?
 		  echo " $0: orphan lock file warning. Lock file deleted."
-		  rm /tmp/program.lock
+#rm /tmp/program.lock
+		  echo $$ > /tmp/program.lock
       rsync -ahHv --log-file=/root/rlog --delete-after \
         --exclude "dapper*" --exclude "hardy*" --exclude "intrepid*" --exclude "jaunty*" --exclude "maverick*"\
       ${RSYNCSOURCE} ${BASEDIR}
   fi
+else
+		  echo the file does not exist
+		  echo " $0: orphan lock file warning. Lock file deleted."
+#rm /tmp/program.lock
+		  echo $$ > /tmp/program.lock
+      rsync -ahHv --log-file=/root/rlog --delete-after \
+        --exclude "dapper*" --exclude "hardy*" --exclude "intrepid*" --exclude "jaunty*" --exclude "maverick*"\
+      ${RSYNCSOURCE} ${BASEDIR}
 fi
 
-rm /tmp/program.lock
+#rm /tmp/program.lock
