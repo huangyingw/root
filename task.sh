e61bd47a7a51e08dd68273e32d78f4f5ba48c253
huangyingw@linux_programming <huangyingw@gmail.com>
2011-03-20 05:01:38 -0400

    n

--- task.sh
+++ task.sh
@@ -8,6 +8,10 @@ then
   /root/myproject/git/linux/bashrc/mirror.sh /media/volgrp/cartoon/ /media/750G/cartoon/
   /root/myproject/git/linux/bashrc/mirror.sh /media/volgrp/movie/ /media/750G/movie
   /root/myproject/git/linux/bashrc/mirror.sh /media/volgrp/storage/ /media/750G/storage
+  /root/myproject/git/linux/bashrc/mirror.sh "/media/volgrp/av/Erika Sato 佐藤江梨花/" "/media/750G/av/Erika Sato 佐藤江梨花/"
+  /root/myproject/git/linux/bashrc/mirror.sh "/media/volgrp/av/Sakura Kiryu 桐生/" "/media/750G/av/Sakura Kiryu 桐生/"
+  /root/myproject/git/linux/bashrc/mirror.sh "/media/volgrp/av/Yuna Takizawa 滝沢優奈/" "/media/750G/av/Yuna Takizawa 滝沢優奈/"
+
   if [ "$1" != s ];
   then
     shutdown -h now
